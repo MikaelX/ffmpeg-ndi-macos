b65fe4937dc3e59825f5b068f60ddada6f65b41c
Pierre-Yves MERLE <p.merle@netfreaks.fr>
2020-07-30 23:38:09 +0200

    add log to artifact

--- compile.sh
+++ compile.sh
@@ -41,12 +41,12 @@ sudo make install
 mkdir $PROJDIR/ffmpeg_sources
 mkdir $PROJDIR/bin
 
-# cd $PROJDIR/ffmpeg_sources
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-# sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-# sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-# sudo apt-get update
-# sudo apt-get install -y cuda cuda-npp-10-0 cuda-npp-dev-10-0 
+cd $PROJDIR/ffmpeg_sources
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install -y cuda cuda-npp-10-0 cuda-npp-dev-10-0 
 
 wget https://cloud.netfreaks.fr/s/NTTX7Qoycee334j/download -O NDI.tgz
 tar xf NDI.tgz
